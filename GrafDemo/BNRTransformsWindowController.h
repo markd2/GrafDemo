0cbd217dee3af80acaa39fda3173546625474082
Mark Dalrymple <markd@bignerdranch.com>
2015-07-07 13:15:31 -0400

    Added stub transformer window

---
+++ GrafDemo/BNRTransformsWindowController.h
@@ -0,0 +1,13 @@
+//
+//  BNRTransformsWindowController.h
+//  GrafDemo
+//
+//  Created by Mark Dalrymple on 7/7/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BNRTransformsWindowController : NSWindowController
+
+@end
---
+++ GrafDemo/BNRTransformsWindowController.m
@@ -0,0 +1,23 @@
+//
+//  BNRTransformsWindowController.m
+//  GrafDemo
+//
+//  Created by Mark Dalrymple on 7/7/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRTransformsWindowController.h"
+
+@interface BNRTransformsWindowController ()
+
+@end
+
+@implementation BNRTransformsWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
