e05e8ff586612c2f9b14ae02bf4dc861a246ddcb
markd2 <markd@bignerdranch.com>
2018-01-30 13:57:01 -0500

    Removed 10.9 context cruft

--- GrafDemo/NSView+CGStuff.swift
+++ GrafDemo/NSView+CGStuff.swift
@@ -3,14 +3,8 @@ import Cocoa
 extension NSView {
 
     var currentContext : CGContext {
-        get {
-            // 10.9 doesn't have a reasonable call for getting the current context
-            // that doesn't require jumping through ever-changing opaque pointers.
-            
-            let context = NSGraphicsContext.current()
-
-            return context!.cgContext
-        }
+        let context = NSGraphicsContext.current()
+        return context!.cgContext
     }
     
     func protectGState(_ drawStuff : () -> Void) {
